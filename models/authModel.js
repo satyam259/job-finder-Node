cacb29a82999d24d05e0a8cd9ca22a29c87547c1
satyam259 <155159540+satyam259@users.noreply.github.com>
2025-06-07 13:40:03 +0530

    Update authModel.js

--- models/authModel.js
+++ models/authModel.js
@@ -60,6 +60,31 @@ authSchema.methods.getResetPasswordToken = function () {
   return resetToken;
 };
 
+// userSchema.methods.generateAccessToken = function () {
+//   return jwt.sign(
+//     {
+//       _id: this._id,
+//       email: this.email,
+//       username: this.username,
+//       fullName: this.fullName,
+//     },
+//     process.env.ACCESS_TOKEN_SECRET,
+//     {
+//       expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+//     }
+//   );
+// };
+// userSchema.methods.generateRefreshToken = function () {
+//   return jwt.sign(
+//     {
+//       _id: this._id,
+//     },
+//     process.env.REFRESH_TOKEN_SECRET,
+//     {
+//       expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+//     }
+//   );
+// };
 const Auth = mongoose.model("Auth", authSchema);
 
 module.exports = Auth;
